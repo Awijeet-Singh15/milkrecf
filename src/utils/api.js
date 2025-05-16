1ffaec7294900fa51765dbe1b074f595969b0f93
Awijeet-Singh15 <166923941+Awijeet-Singh15@users.noreply.github.com>
2025-05-17 02:05:42 +0530

    minor changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,13 +2,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://milkrecb.vercel.app/api", 
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`; // <-- Important: Add 'Bearer '
+    config.headers.Authorization = `Bearer ${token}`; 
   }
   return config;
 });
